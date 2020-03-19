031a81d89c4b00b3edde5f4ac3eb0f263b93fd30
nab5m <sk990420@naver.com>
2020-03-19 20:25:30 +0900

    Banner 컴포넌트 스타일링
    - 배경 이미지 추가 필요

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,17 @@
 import React from "react";
+import classNames from 'classnames';
+import './css/Banner.css';
 
 const Banner = () => {
     return (
-        <h1>배너</h1>
+        <section className={classNames("banner")}>
+            <div className={classNames("banner-title", "white")}>
+                웹툰작가를 연결하다. 커넥툰
+            </div>
+            <div className={classNames("banner-description", "aqua-marine")}>
+                connect <span className="white">+</span> web
+            </div>
+        </section>
     );
 };
 
