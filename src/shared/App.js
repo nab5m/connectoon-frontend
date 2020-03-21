112b2a7e517716e0723b56deae12cd4dc69de923
nab5m <sk990420@naver.com>
2020-03-22 00:47:55 +0900

    라우트를 Switch 컴포넌트로 감쌈

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Redirect, Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import {Login, Register} from "../pages";
 import Home from "../pages/Home";
 import {observer} from "mobx-react";
@@ -12,18 +12,20 @@ function App() {
 
     return (
         <div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login">
-                {
-                    loginRegisterStore.token ? <Redirect to="/" /> : <Login />
-                }
-            </Route>
-            <Route exact path="/register">
-                {
-                    loginRegisterStore.token ? <Redirect to="/" /> : <Register />
-                }
-            </Route>
-            <Route path='*' component={Custom404} />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login">
+                    {
+                        loginRegisterStore.token ? <Redirect to="/" /> : <Login />
+                    }
+                </Route>
+                <Route exact path="/register">
+                    {
+                        loginRegisterStore.token ? <Redirect to="/" /> : <Register />
+                    }
+                </Route>
+                <Route path='*' component={Custom404} />
+            </Switch>
         </div>
     );
 }
