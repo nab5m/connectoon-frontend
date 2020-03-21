00491e3d852dcdd3ac018f7a73b52a13c5cfb6f2
nab5m <sk990420@naver.com>
2020-03-22 03:03:03 +0900

    react-cookie 삭제

--- src/client/Root.js
+++ src/client/Root.js
@@ -2,20 +2,17 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import { Provider } from 'mobx-react';
 import App from "../shared/App";
-import {CookiesProvider} from "react-cookie";
 import LoginRegisterStore from "../stores/LoginRegisterStore";
 
 const loginRegisterStore = new LoginRegisterStore();
 
 const Root = () => {
     return (
-        <CookiesProvider >
             <Provider loginRegisterStore={loginRegisterStore}>
                 <BrowserRouter>
                     <App/>
                 </BrowserRouter>
             </Provider>
-        </CookiesProvider>
     );
 };
 
