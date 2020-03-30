813dcefa36279c6ba0d6911fce1e7aa9a80e53e6
nab5m <sk990420@naver.com>
2020-03-30 12:40:37 +0900

    Add StoryCardList UI

--- src/components/view-work/StoryCardItem.js
+++ src/components/view-work/StoryCardItem.js
@@ -1,8 +1,35 @@
 import React from "react";
+import classNames from 'classnames';
+import styles from './css/StoryCardItem.module.css';
 
-const StoryCardItem = () => {
+const StoryCardItem = ({chapter, title, subCategory, description, backgroundImageUrl, profileImageUrl, authorName,
+                           likeCount, replyCount}) =>
+{
     return (
-        <div>StoryCardItem</div>
+        <div className={classNames("col-xl-3", "col-lg-4", "col-md-6")}>
+            <div className={styles.container} style={{backgroundImage: `url(${backgroundImageUrl})`}}>
+                <div className={styles.workIntro}>
+                    <p>{chapter}</p>
+                    <h3>{title}</h3>
+                </div>
+                <hr className={styles.division} />
+
+                {/* <script> 태그와 <a> 태그의 삽입 공격에 유의할 것 */}
+                <div className={styles.workDescription} dangerouslySetInnerHTML={{ __html: description }}>
+                </div>
+                <div className={styles.authorProfile}>
+                    <img src={ profileImageUrl } alt="프로필_사진"/>
+                    <span>{authorName} 작가</span>
+                </div>
+            </div>
+            <div className={styles.cardFooter}>
+                <img src="/images/heart_select.svg" alt="좋아요" />
+                <span className={styles.likeCount}>{likeCount}</span>
+
+                <img src="/images/reply.svg" alt="댓글" />
+                <span>{replyCount >= 1000 ? '999+' : replyCount}</span>
+            </div>
+        </div>
     );
 };
 
--- src/components/view-work/StoryCardList.js
+++ src/components/view-work/StoryCardList.js
@@ -1,13 +1,23 @@
-import React, {Fragment} from "react";
+import React from "react";
 import StoryCardItem from "./StoryCardItem";
 import WorkPaging from "./WorkPaging";
+import styles from './css/StoryCardList.module.css';
 
 const StoryCardList = () => {
     return (
-        <Fragment>
-            <StoryCardItem />
+        <div className={styles.container}>
+            <StoryCardItem
+                chapter="01"
+                title="치즈인더트랩"
+                description="평범한 대학생 홍설,<br> 평범하지 않은 유정을 만나 .."
+                backgroundImageUrl="https://cdn.zeplin.io/5e4e618501e9837d3d37cde2/assets/11E0C15F-CFF6-4D91-BAE5-1050AFC2E9CA.png"
+                profileImageUrl="https://cdn.zeplin.io/5e4e618501e9837d3d37cde2/assets/E0AA3F1C-F1F6-4341-B311-02E2B3DEC99B.png"
+                authorName="망난희"
+                likeCount={100}
+                replyCount={1024}
+            />
             <WorkPaging />
-        </Fragment>
+        </div>
     );
 };
 
