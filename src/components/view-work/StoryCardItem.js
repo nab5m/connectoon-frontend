3d035056ee48407c8f4ce2740900531534a99221
nab5m <sk990420@naver.com>
2020-03-31 11:33:57 +0900

    Add responsive StoryCard UI

--- src/components/view-work/StoryCardItem.js
+++ src/components/view-work/StoryCardItem.js
@@ -6,20 +6,22 @@ const StoryCardItem = ({chapter, title, subCategory, description, backgroundImag
                            likeCount, replyCount}) =>
 {
     return (
-        <div className={classNames("col-xl-3", "col-lg-4", "col-md-6")}>
+        <div className={classNames("col-xl-3", "col-lg-4", "col-md-6", "mb-4")}>
             <div className={styles.container} style={{backgroundImage: `url(${backgroundImageUrl})`}}>
-                <div className={styles.workIntro}>
-                    <p>{chapter}</p>
-                    <h3>{title}</h3>
-                </div>
-                <hr className={styles.division} />
+                <div className={styles.wrapper}>
+                    <div className={styles.workIntro}>
+                        <p>{chapter}</p>
+                        <h3>{title}</h3>
+                    </div>
+                    <hr className={styles.division} />
 
-                {/* <script> 태그와 <a> 태그의 삽입 공격에 유의할 것 */}
-                <div className={styles.workDescription} dangerouslySetInnerHTML={{ __html: description }}>
-                </div>
-                <div className={styles.authorProfile}>
-                    <img src={ profileImageUrl } alt="프로필_사진"/>
-                    <span>{authorName} 작가</span>
+                    {/* <script> 태그와 <a> 태그의 삽입 공격에 유의할 것 */}
+                    <div className={styles.workDescription} dangerouslySetInnerHTML={{ __html: description }}>
+                    </div>
+                    <div className={styles.authorProfile}>
+                        <img src={ profileImageUrl } alt="프로필_사진"/>
+                        <span>{authorName} 작가</span>
+                    </div>
                 </div>
             </div>
             <div className={styles.cardFooter}>
